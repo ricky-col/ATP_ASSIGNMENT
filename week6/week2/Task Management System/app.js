125a25f0fa3242464b96a33c8a3b413da3aa46b6
Revanth Tellaboina <trevanth61@gmail.com>
2026-02-19 19:55:49 +0530

    Delete week6/week2 directory

--- week6/week2/Task Management System/app.js
+++
@@ -1,7 +0,0 @@
-import { addTask, getAllTasks, completeTask } from './task.js';
-console.log(addTask("Buy Milk", "high", "2026-12-01"));
-console.log(addTask("JS Practice", "medium", "2026-12-05"));
-console.log("All Tasks:", getAllTasks());
-const firstTaskId = getAllTasks()[0].id;
-console.log(completeTask(firstTaskId));
-console.log("Updated Tasks:", getAllTasks());

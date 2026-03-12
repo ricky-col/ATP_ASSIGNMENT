b781bc8ea82cd9abb9a3d98eb655cad97e2fcdc6
Revanth Tellaboina <trevanth61@gmail.com>
2026-03-12 19:04:29 +0530

    Delete week7/react-handson1 directory

--- week7/react-handson1/src/components/Contactus.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Contactus() {
-  return (
-    <div>Contact_us</div>
-  )
-}
-
-export default Contactus
--- week7/react-handson1/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-  return (
-    <div className='text-center text-4xl bg-amber-400 p-2'>Footer</div>
-  )
-}
-
-export default Footer
